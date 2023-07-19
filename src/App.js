4e447255bfdc9b220e74788531b4e9d745c905c6
Vanshika <vanshikabhojnagarwala@gmail.com>
2023-07-19 15:12:20 +0530

    style: add styling for table

--- src/App.js
+++ src/App.js
@@ -41,35 +41,37 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="App w-[100vw] min-h-[100vh] bg-[#F8EEFF] flex flex-col justify-start items-center p-5">
+      <h1 className="text-2xl text-[#dba6f5] font-semibold">Pagination in React</h1>
       <input
         type="number"
-        placeholder="Items per Page..."
+        placeholder="Enter number of items per page"
         onChange={handleItemsPerPageChange}
-        value={itemsPerPage}
+        //value={itemsPerPage}
+        className="w-1/4 bg-[#F8EEFF] border-b-2 border-[#EDC8FF] p-2 m-2 focus:outline-none"
       />
-      <table>
+      <table className="w-1/3 bg-slate-50 p-2 m-2">
         <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Price</th>
+          <tr className="bg-[#EDC8FF]">
+            <th className="p-2">ID</th>
+            <th className="p-2">Name</th>
+            <th className="p-2">Price</th>
           </tr>
         </thead>
         <tbody>
           {items.map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.title}</td>
-              <td>{item.price}</td>
+            <tr key={item.id} className=" border-b-2 border-[#EDC8FF]">
+              <td className="p-2">{item.id}</td>
+              <td className="p-2">{item.title}</td>
+              <td className="p-2">{item.price}</td>
             </tr>
           ))}
         </tbody>
       </table>
-      <div>
-        <button onClick={handlePrev}>Prev</button> 
+      <div className="flex w-20 justify-around p-2 m-2 text-[#dba6f5] font-semibold">
+        <button onClick={handlePrev}>&lt; </button>
         <p>{currentPage}</p>
-        <button onClick={handleNext}>Next</button>
+        <button onClick={handleNext}>&gt;</button>
       </div>
     </div>
   );
